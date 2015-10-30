c47660216d7723e6c34bca5acd9379217b13d403
potaka <potaka@debian>
2015-10-31 00:51:07 +0200

    wrong table name

--- migration/migrations/20150705073208_lyric_table.php
+++ migration/migrations/20150705073208_lyric_table.php
@@ -14,7 +14,7 @@ class LyricTable extends AbstractMigration
         $table->renameColumn('zaglavie_palno', 'cache_title_full');
         $table->renameColumn('zaglavie_sakrateno', 'cache_title_short');
         
-        $table->renameColumn('dopylnitelnoinfo', 'extraInfo');
+        $table->renameColumn('dopylnitelnoinfo', 'extra_info');
         $this->query("ALTER TABLE `lyric` CHANGE `extra_info` `extra_info` TEXT CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL; ");
         
         $this->query('ALTER TABLE `lyric` CHANGE `video_youtube` `video_youtube` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL;');
