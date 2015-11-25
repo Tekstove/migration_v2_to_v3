5a3aa5381ba7dec14cd1eff513cf1c4ea63dc421
po_taka <angel.koilov@gmail.com>
2015-11-26 00:38:56 +0200

    mapping user groups

---
+++ migration/migrations/00000000002140_permission_group_user.php
@@ -0,0 +1,18 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class PermissionGroupUser extends AbstractMigration
+{
+    public function up()
+    {
+        $this->query("ALTER TABLE permission_group_users DROP FOREIGN KEY permission_group_users_ibfk_1;");
+        $this->query("ALTER TABLE permission_group_users DROP FOREIGN KEY permission_group_users_ibfk_2;");
+        
+        $this->query("ALTER TABLE `permission_group_users` CHANGE `groupId` `group_Id` INT(10) UNSIGNED NOT NULL; ");
+        $this->query("ALTER TABLE `permission_group_users` CHANGE `userId` `user_id` INT(11) UNSIGNED NOT NULL; ");
+        
+        $this->query("ALTER TABLE `permission_group_users` ADD FOREIGN KEY (`group_Id`) REFERENCES `permission_group`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; ");
+        $this->query("ALTER TABLE `permission_group_users` ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT");
+    }
+}
