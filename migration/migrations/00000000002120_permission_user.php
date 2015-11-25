2eca108d0ee3fb598f94226aade3ca1d5626f7da
po_taka <angel.koilov@gmail.com>
2015-11-25 23:52:48 +0200

    case type

--- migration/migrations/00000000002120_permission_user.php
+++ migration/migrations/00000000002120_permission_user.php
@@ -11,9 +11,9 @@ class PermissionUser extends AbstractMigration
         $this->query("ALTER TABLE permission_user DROP FOREIGN KEY permission_user_ibfk_1");
         echo "FKs removed\n";
         $this->query("ALTER TABLE `permission_user` CHANGE `permissionId` `permission_id` INT(10) UNSIGNED NOT NULL;");
-        $this->query("ALTER TABLE `permission_user` CHANGE `userId` `user_Id` INT(10) UNSIGNED NOT NULL; ");
+        $this->query("ALTER TABLE `permission_user` CHANGE `userId` `user_id` INT(10) UNSIGNED NOT NULL; ");
         
         $this->query("ALTER TABLE `permission_user` ADD FOREIGN KEY (`permission_id`) REFERENCES `permission`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT");
-        $this->query("ALTER TABLE `permission_user` ADD FOREIGN KEY (`user_Id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT");
+        $this->query("ALTER TABLE `permission_user` ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT");
     }
 }
