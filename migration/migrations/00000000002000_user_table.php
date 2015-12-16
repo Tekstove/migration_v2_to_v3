17861968c179433aa1c267b4713e1e6289f08815
angelk <angel.koilov@gmail.com>
2015-12-16 21:15:09 +0200

    user table changes

--- migration/migrations/00000000002000_user_table.php
+++ migration/migrations/00000000002000_user_table.php
@@ -6,6 +6,26 @@ class UserTable extends AbstractMigration
 {
     public function up()
     {
-        $this->table('users')->rename('user');
+        $table = $this->table('users');
+        $table->rename('user');
+        $table->removeColumn('class');
+        $table->renameColumn('rajdane', 'birthday');
+        $this->query("ALTER TABLE `user` CHANGE `rajdane` `rajdane` VARCHAR(8) CHARACTER SET utf8 COLLATE utf8_bin NULL; ");
+        $this->query("UPDATE user set rajdane = NULL where rajdane = ''");
+        $this->query("ALTER TABLE `user` ADD `birthday` DATE NULL DEFAULT NULL AFTER `rajdane`; ");
+        $this->query("
+            UPDATE
+                user
+            SET
+            birthday = DATE(
+                CONCAT(
+                    substring(rajdane,1,4),
+                    SUBSTRING(rajdane, 5, 2),
+                    SUBSTRING(rajdane,7,2)
+                )
+            )
+            WHERE rajdane IS NOT NULL
+        ");
+        $table->removeColumn('rajdane');
     }
 }
