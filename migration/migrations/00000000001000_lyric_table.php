d5a759cdd719b9aca9cadee1f5b31221614d397b
angelk <angel.koilov@gmail.com>
2016-02-01 10:21:21 +0200

    user table changes

--- migration/migrations/00000000001000_lyric_table.php
+++ migration/migrations/00000000001000_lyric_table.php
@@ -18,7 +18,6 @@ class LyricTable extends AbstractMigration
         $table->renameColumn('populqrnost', 'popularity');
         $table->renameColumn('vidqna', 'views');
         $table->renameColumn('glasa', 'votes_count');
-        $table->renameColumn('up_id', 'uploaded_by');
         $this->query("ALTER TABLE `lyric` CHANGE `uploaded_by` `uploaded_by` INT(11) UNSIGNED NULL DEFAULT NULL; ");
         $table->renameColumn('zaglavie_palno', 'cache_title_full');
         $table->renameColumn('zaglavie_sakrateno', 'cache_title_short');
@@ -32,7 +31,7 @@ class LyricTable extends AbstractMigration
         
         $this->query('ALTER TABLE `lyric` CHANGE `video_vbox7` `video_vbox7` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL; ');
         
-        $this->query('ALTER TABLE `lyric` CHANGE `uploaded_by` `user_id` INT(11) UNSIGNED NULL DEFAULT NULL;');
+        $this->query('ALTER TABLE `lyric` CHANGE `up_id` `send_by` INT(11) UNSIGNED NULL DEFAULT NULL;');
         
         $this->query("
             ALTER TABLE lyric MODIFY text_bg text NULL;
